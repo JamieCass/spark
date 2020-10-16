0453748a9b6d504607e008d7dc35137010c12b62
JamieCass <jamiecass92@gmail.com>
2020-10-16 17:13:55 +1000

    new dates and timestamps file

--- missing_data.py
+++ missing_data.py
@@ -23,4 +23,24 @@ df.na.drop(subset=['Sales']).show() # this drops any rows that have a null in th
 
 
 # ------------------------------ FILLING DATA ------------------------------
-# i will get to this when i have some spare time
+# to fill everything with a string (sspark will fill all fields that have values as a string )
+df.na.fill('FILL VALUE').show()
+
+# to fill in everything with a numeric value (spark will again fill in any fields that have a numeric value)
+df.na.fill(0).show()
+
+
+# to spicfy what fields/colums you want to fill (declare the subset)
+df.na.fill('No Name', subset = ['Name']).show()
+
+#-------------- common practice --------------
+# fill all numerice values with the mean value
+# first import the function
+from pyspark.sql.functions import mean
+
+mean_val = df.select(mean(df['Sales'])).collect()
+mean_sales = mean_val[0][0] # remember we need to index because it will return a list
+df.na.fill(mean_sales, ['Sales']).show()
+
+# to put it all on 1 line
+df.na.fill(df.select(mean(df['Sales'])).collect()[0][0], ['Sales']).show()
